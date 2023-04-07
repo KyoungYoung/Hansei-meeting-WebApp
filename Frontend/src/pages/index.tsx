15a86d2df5c2e6bb0bbc21a12d689f87ab6274a7
mudokja <90081875+mudokja@users.noreply.github.com>
2023-04-07 14:20:38 +0900

    init: front setting

---
+++ Frontend/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Test Next App</title>
+        <meta name="description" content="test page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <div>
+          hello~~~~~ world!!!!!!!!!!
+        </div>
+      </main>
+    </>
+  )
+}
