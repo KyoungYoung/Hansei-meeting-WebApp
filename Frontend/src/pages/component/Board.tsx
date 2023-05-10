891080d5da6c261bac932edc222a689e1b72f397
park <kpdj4588@gmail.com>
2023-05-10 14:24:50 +0900

    날짜변경

---
+++ Frontend/src/pages/component/Board.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+const Test = () => {
+    return ( <div>hello</div> );
+}
+ 
+export default Test;
+
--- Frontend/src/pages/index.tsx
+++ Frontend/src/pages/index.tsx
@@ -1,18 +1,17 @@
-import Head from 'next/head'
+import Test from "./component/test";
+import styled from 'styled-components';
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Test Next App</title>
-        <meta name="description" content="test page" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <main>
-        <div>
-          hello~~~~~ world!!!!!!!!!!
-        </div>
-      </main>
-    </>
-  )
+
+
+
+function Main () {
+  return (  
+    <div>
+      <Test/>
+    </div>
+   
+    
+   );
 }
+
+export default Main ;
